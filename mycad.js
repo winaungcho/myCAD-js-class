a8f8e183bb5db45334f493c43082a65fc4928a47
Win Aung Cho <54325207+winaungcho@users.noreply.github.com>
2020-11-22 21:07:33 +0630

    Add files via upload

--- mycad.js
+++ mycad.js
@@ -297,3 +297,161 @@ class myCAD {
 	}
 	
 }
+function drawJoint(ctx, mycad) {
+		var D1 = 40,
+			ED = 75,
+			S = 85,
+			tp = 22,
+			ts = 13,
+			td = 9.5,
+			Bp = 200,
+			db = 22,
+			g = 100;
+		var lfac = 1;
+		var boltType = "A325";
+		var beamBotSlope = 1.0 / 4;
+		var x = 350,
+			y = 350;
+		var Beam = {
+				"d": 400,
+				"tw": 10,
+				"bf": 154,
+				"tf": 13,
+				"k": 24
+			}
+			//Column:
+		var C = {
+			"d": 350,
+			"tw": 14,
+			"bf": 315,
+			"tf": 20,
+			"k": 31,
+			"k1": 34
+		}
+
+		
+		var elmts = document.getElementsByTagName("input");
+		var L = elmts.length;
+		for(var i = 0; i < L; i++) {
+			try {
+				eval(elmts[i].name + "=" + elmts[i].value);
+			} catch(e) {}
+		}
+		var lp = 2 * (D1 + ED) + Beam.d;
+		
+		ctx.save();
+		ctx.setTransform(0.5 * lfac, 0, 0, 0.5 * lfac, 0, 0);
+		ctx.translate(x / lfac, y / lfac);
+		ctx.fillStyle = "rgba(200, 255, 255, 0.4)";
+		ctx.strokeStyle = "#555";
+		ctx.setLineDash(mycad.linePattern.dashdot2);
+		ctx.lineWidth = 2;
+		mycad.drawLine(ctx, 0, -(D1 + ED + 150), 0, lp + 150);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, -D1, C.d / 2 + tp + 25, -D1);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, -D1 + S, C.d / 2 + tp + 25, -D1 + S);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, Beam.d + D1, C.d / 2 + tp + 25, Beam.d + D1);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, Beam.d + D1 - S, C.d / 2 + tp + 25, Beam.d + D1 - S);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, Beam.d / 2 - S / 2, C.d / 2 + tp + 25, Beam.d / 2 - S / 2);
+		mycad.drawLine(ctx, C.d / 2 - C.tf - 25, Beam.d / 2 + S / 2, C.d / 2 + tp + 25, Beam.d / 2 + S / 2);
+		ctx.setLineDash(mycad.linePattern.cont);
+		ctx.strokeStyle = "#000";
+		ctx.lineWidth = 3;
+		mycad.drawLine(ctx, -C.d / 2, -(D1 + ED + 100), -C.d / 2, lp + 100);
+		mycad.drawLine(ctx, -C.d / 2 + C.tf, -(D1 + ED + 100), -C.d / 2 + C.tf, lp + 100);
+		mycad.drawLine(ctx, C.d / 2, -(D1 + ED + 100), C.d / 2, lp + 100);
+		mycad.drawLine(ctx, C.d / 2 - C.tf, -(D1 + ED + 100), C.d / 2 - C.tf, lp + 100);
+		mycad.drawHContinueLine(ctx, 0, -(D1 + ED + 100), C.d + 200);
+		mycad.drawHContinueLine(ctx, 0, (lp + 100), C.d + 200);
+		mycad.drawFillRect(ctx, 0 - C.d / 2 + C.tf, 0, C.d - 2 * C.tf, ts);
+		mycad.drawFillRect(ctx, 0 - C.d / 2 + C.tf, Beam.d - ts, C.d - 2 * C.tf, ts);
+		mycad.centerText(ctx, 0, Beam.d + 120, "Column", 48 / lfac);
+		mycad.centerText(ctx, 0, Beam.d + 180, mycad.sectionName(C), 48 / lfac);
+		mycad.weldLeader(ctx, 0, Beam.tf, 100, 300, 100, 6, "E70 SMAW", 0, Beam.d - 2 * ts);
+		mycad.drawRect(ctx, C.d / 2, -(D1 + ED), tp, lp);
+		mycad.drawLineWithArrows(ctx, C.d / 2 + tp, Beam.d / 2, C.d / 2 + tp + Beam.d, -ED - D1, 10, 24, true, false);
+		mycad.drawLineWithArrows(ctx, C.d / 2 + tp + Beam.d * 2 + C.bf / 2 - Bp / 2, Beam.d / 2, C.d / 2 + tp + Beam.d, -ED - D1, 10, 24, true, false);
+		mycad.centerText(ctx, C.d / 2 + tp + Beam.d, -ED - D1, "End Plate " + tp + " mm thick", 48 / lfac);
+		mycad.weldLeader(ctx, C.d / 2 + tp, Beam.d - 100, 200, 200, 200, 6, "E70 SMAW");
+		var dy = Beam.d * 2 * beamBotSlope;
+		mycad.drawLine(ctx, C.d / 2 + tp, 0, C.d / 2 + tp + Beam.d * 2, 0);
+		mycad.drawLine(ctx, C.d / 2 + tp, Beam.tf, C.d / 2 + tp + Beam.d * 2, Beam.tf);
+		mycad.drawLine(ctx, C.d / 2 + tp, Beam.d, C.d / 2 + tp + Beam.d * 2, Beam.d - dy);
+		mycad.drawLine(ctx, C.d / 2 + tp, Beam.d - Beam.tf, C.d / 2 + tp + Beam.d * 2, Beam.d - Beam.tf - dy);
+		mycad.centerText(ctx, C.d / 2 + tp + Beam.d, Beam.d / 2, "Beam", 48 / lfac);
+		mycad.centerText(ctx, C.d / 2 + tp + Beam.d, Beam.d / 2 + 60, mycad.sectionName(Beam), 48 / lfac);
+		mycad.drawContinueLine(ctx, C.d / 2 + tp + Beam.d * 2, Beam.d + 100 - dy, C.d / 2 + tp + Beam.d * 2, -100);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, -D1 - db / 2, C.tf + tp, db);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, -D1 + S - db / 2, C.tf + tp, db);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, Beam.d + D1 - db / 2, C.tf + tp, db);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, Beam.d + D1 - S - db / 2, C.tf + tp, db);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, Beam.d / 2 - S / 2 - db / 2, C.tf + tp, db);
+		mycad.drawFillRect(ctx, C.d / 2 - C.tf, Beam.d / 2 + S / 2 - db / 2, C.tf + tp, db);
+		//col section
+		var xc = 0,
+			yc = lp + 100 + C.bf;
+		mycad.Hsection(ctx, 0, yc, 90, C.bf, C.d, C.tw, C.tf, 10);
+		mycad.drawRect(ctx, xc + C.d / 2, yc - Bp / 2, tp, Bp);
+		mycad.drawFillRect(ctx, xc + C.d / 2 - C.tf, yc - g / 2 - db / 2 - 4, C.tf + tp, db);
+		mycad.drawFillRect(ctx, xc + C.d / 2 - C.tf, yc + g / 2 - db / 2 - 4, C.tf + tp, db);
+		mycad.drawFillRect(ctx, xc - C.d / 2 + C.tf + 6, yc - C.tw / 2 - td, C.d - 2 * (C.tf + 6), td);
+		mycad.drawFillRect(ctx, xc - C.d / 2 + C.tf + 6, yc + C.tw / 2, C.d - 2 * (C.tf + 6), td);
+		mycad.drawLeader(ctx, xc + C.d / 4, yc + C.tw / 2 + td, 0, C.d, C.bf / 2 + 20, "Web doubler plate " + td + " mm thick");
+		mycad.drawLine(ctx, xc - C.d / 2 + C.tf, yc - Bp / 2, xc + C.d / 2 + C.tf, yc - Bp / 2);
+		mycad.drawLine(ctx, xc - C.d / 2 + C.tf, yc + Bp / 2, xc + C.d / 2 + C.tf, yc + Bp / 2);
+		mycad.drawLine(ctx, xc + C.d / 2 + tp, yc + Beam.bf / 2, xc + C.d / 2 + tp + Beam.d * 2, yc + Beam.bf / 2);
+		mycad.drawLine(ctx, xc + C.d / 2 + tp, yc - Beam.bf / 2, xc + C.d / 2 + tp + Beam.d * 2, yc - Beam.bf / 2);
+		mycad.drawContinueLine(ctx, xc + C.d / 2 + tp + Beam.d * 2, yc + Beam.bf / 2 + 100, xc + C.d / 2 + tp + Beam.d * 2, yc - Beam.bf / 2 - 100);
+		mycad.centerText(ctx, C.d / 2 + tp + Beam.d, yc - 30, "Beam", 48 / lfac);
+		mycad.centerText(ctx, C.d / 2 + tp + Beam.d, yc + 30, mycad.sectionName(Beam), 48 / lfac);
+		mycad.drawLineWithArrows(ctx, xc, yc - Bp / 2, xc + C.d / 2 + tp, yc - C.bf / 2 - 80, 10, 24, true, false);
+		mycad.drawLine(ctx, xc + C.d / 2 + tp, yc - C.bf / 2 - 80, xc + C.d / 2 + 120, yc - C.bf / 2 - 80);
+		mycad.leftText(ctx, xc + C.d / 2 + 120, yc - C.bf / 2 - 80, "Stiffener " + ts + " mm thick", 48 / lfac);
+		mycad.drawRoDim(ctx, xc - C.d / 2, yc + C.bf / 2, xc + C.d / 2, yc + C.bf / 2, 80);
+		mycad.drawRoDim(ctx, xc + C.d / 2, yc + Bp / 2, xc + C.d / 2, yc - Bp / 2, 80);
+		mycad.drawRoDim(ctx, xc + C.d / 2, yc + C.bf / 2, xc + C.d / 2, yc - C.bf / 2, 120);
+		//beam section
+		var xc = tp + 50 + C.d / 2 + Beam.d * 2 + C.bf / 2;
+		var yc = Beam.d / 2;
+		mycad.drawLine(ctx, xc - C.bf / 2, yc - Beam.d / 2 - (D1 + ED + 100), xc - C.bf / 2, yc - Beam.d / 2 + lp + 100);
+		mycad.drawLine(ctx, xc + C.bf / 2, yc - Beam.d / 2 - (D1 + ED + 100), xc + C.bf / 2, yc - Beam.d / 2 + lp + 100);
+		mycad.Hsection(ctx, xc, Beam.d / 2, 0, Beam.bf, Beam.d, Beam.tw, Beam.tf, 10);
+		mycad.drawRect(ctx, xc - Bp / 2, -D1 - ED, Bp, lp);
+		mycad.drawRoDim(ctx, xc - Bp / 2, yc + lp / 2, xc + Bp / 2, yc + lp / 2, 80);
+		mycad.centerText(ctx, xc, yc + lp / 2 + D1 + ED + 160, "End Plate " + Bp + "x" + lp + "x" + tp, 48 / lfac);
+		mycad.centerText(ctx, xc, yc + lp / 2 + D1 + ED + 220, boltType + " Type " + db + " Φmm Bolts", 48 / lfac);
+		var vdimL = (C.bf - Bp) / 2 + 80;
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc + lp / 2, xc + Bp / 2, yc + lp / 2 - ED, vdimL);
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc + lp / 2 - ED, xc + Bp / 2, yc + lp / 2 - ED - S, vdimL);
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc + lp / 2 - ED - S, xc + Bp / 2, yc - lp / 2 + ED + S, vdimL);
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc - lp / 2 + ED + S, xc + Bp / 2, yc - lp / 2 + ED, vdimL);
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc - lp / 2 + ED, xc + Bp / 2, yc - lp / 2, vdimL);
+		mycad.drawRoDim(ctx, xc + Bp / 2, yc + lp / 2, xc + Bp / 2, yc - lp / 2, vdimL + 40);
+		mycad.drawHContinueLine(ctx, xc, yc - Beam.d / 2 - (D1 + ED + 100), C.bf + 200);
+		mycad.drawHContinueLine(ctx, xc, yc + lp / 2 + D1 + ED + 100, C.bf + 200);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc - Beam.d / 2 - D1, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc - Beam.d / 2 - D1, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc - Beam.d / 2 - D1 + S, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc - Beam.d / 2 - D1 + S, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc - S / 2, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc + S / 2, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc - S / 2, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc + S / 2, db / 2 + 4);
+		mycad.drawRoDim(ctx, xc - g / 2, yc - Beam.d / 2 - D1, xc + g / 2, yc - Beam.d / 2 - D1, -ED - 80);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc + Beam.d / 2 + D1, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc + Beam.d / 2 + D1, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc - g / 2, yc + Beam.d / 2 + D1 - S, db / 2 + 4);
+		mycad.drawFillCircle(ctx, xc + g / 2, yc + Beam.d / 2 + D1 - S, db / 2 + 4);
+		mycad.drawPoint(ctx, xc - g / 2, yc - Beam.d / 2 - D1, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc - Beam.d / 2 - D1, db + 16);
+		mycad.drawPoint(ctx, xc - g / 2, yc - Beam.d / 2 - D1 + S, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc - Beam.d / 2 - D1 + S, db + 16);
+		mycad.drawPoint(ctx, xc - g / 2, yc + Beam.d / 2 + D1, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc + Beam.d / 2 + D1, db + 16);
+		mycad.drawPoint(ctx, xc - g / 2, yc + Beam.d / 2 + D1 - S, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc + Beam.d / 2 + D1 - S, db + 16);
+		mycad.drawPoint(ctx, xc - g / 2, yc - S / 2, db + 16);
+		mycad.drawPoint(ctx, xc - g / 2, yc + S / 2, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc - S / 2, db + 16);
+		mycad.drawPoint(ctx, xc + g / 2, yc + S / 2, db + 16);
+		ctx.restore();
+	}
